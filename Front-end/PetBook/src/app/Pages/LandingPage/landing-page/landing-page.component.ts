36554a5b95d4ce72857dc4e1a3fa6281a9d5df58
salma197457 <salma.ali67815@gmail.com>
2024-06-23 18:23:58 +0300

    Editing Landing Page, Header and Footer

--- Front-end/PetBook/src/app/Pages/LandingPage/landing-page/landing-page.component.ts
+++ Front-end/PetBook/src/app/Pages/LandingPage/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../Core/Header/header/header.component';
 import { RouterLink } from '@angular/router';
 
@@ -9,6 +9,29 @@ import { RouterLink } from '@angular/router';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  images = [
+    'url(../../../../assets/Images/Heroimage1.jpg)', 
+    'url(https://www.usatoday.com/money/blueprint/images/uploads/2023/06/27134316/best-pet-insurance-scaled-e1687873423254.jpg)',  
+    'url(https://www.census.gov/newsroom/stories/pet-day/_jcr_content/root/responsivegrid/responsivegrid_1749353263/imagecore.coreimg.jpeg/1680634248356/stories-pet3-1300x867.jpeg)',  
+    'url(../../../../assets/Images/Heroimage2.jpg)', 
+    'url(https://cottagelife.com/wp-content/uploads/2014/07/shutterstock_1676506852.jpg)',  
+];
+  
+currentIndex = 0;
 
+ngOnInit(): void {
+  this.changeBackground();
+  setInterval(() => this.changeBackground(), 4000);
 }
+changeBackground(): void {
+  const slideshow = document.getElementById('HeroPhoto');
+  if (slideshow) {
+    setTimeout(() => {
+      slideshow.style.backgroundImage = this.images[this.currentIndex];
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 500); 
+  }
+}
+}
+
