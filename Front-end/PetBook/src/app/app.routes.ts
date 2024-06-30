5afd3b384f4bbaa7f8b72fa1685a8c9b31708221
Rana Elgohary <r.elgohary2000@gmail.com>
2024-06-30 05:31:40 +0300

    Guards for client Done

--- Front-end/PetBook/src/app/app.routes.ts
+++ Front-end/PetBook/src/app/app.routes.ts
@@ -14,7 +14,6 @@ import { ClinicComponent } from './Pages/clinic/clinic.component';
 import { UserProfilePetInfoComponent } from './Pages/userPetInfo/user-pet-info/userProfile-pet-info.component';
 import { noNavigateWithoutLoginGuard } from './Guard/no-navigate-without-login.guard';
 import { SearchVaccineComponent } from './Pages/search-vaccine/search-vaccine.component';
-import { VaccineClinic } from './Models/vaccine-clinic';
 import { SearchVaccineClicnicComponent } from './Pages/search-vaccine-clicnic/search-vaccine-clicnic.component';
 import { UserPetInfoEditComponent } from './Pages/user-pet-info-edit/user-pet-info-edit.component';
 import { ReservationForVaccineComponent } from './Pages/reservation-for-vaccine/reservation-for-vaccine.component';
@@ -31,29 +30,29 @@ export const routes: Routes = [
     {path: "UserSignUp", component:UserSignUpComponent, title:"User Sign-Up", canActivate: [noNavigateToLoginPageIfTokenGuard]},
     
     {path: "Profile", component:UserProfileMainComponent, title:"Profile", children:[
-        {path: "", redirectTo: "Account", pathMatch: "full" },
-        {path: "Account", component:UserDetailsComponent, title:"Account"},
-        {path:"pendingRequest", component:PendingRequestComponent , title:"pending Request"},
-        {path: "MyRequest", component:MyRequestComponent, title:"MY Request"},
-        {path: "userPetInfo",component: UserProfilePetInfoComponent, title:"Pet Information"},
-        {path: "Reservations",component: ShowReservationsComponent , title:"Reservatios"},
-    ]},
+        {path: "", redirectTo: "Account", pathMatch: "full"},
+        {path: "Account", component:UserDetailsComponent, title:"Account", canActivate:[noNavigateWithoutLoginGuard]},
+        {path:"pendingRequest", component:PendingRequestComponent , title:"pending Request", canActivate:[noNavigateWithoutLoginGuard]},
+        {path: "MyRequest", component:MyRequestComponent, title:"MY Request", canActivate:[noNavigateWithoutLoginGuard]},
+        {path: "userPetInfo",component: UserProfilePetInfoComponent, title:"Pet Information", canActivate:[noNavigateWithoutLoginGuard]},
+        {path: "Reservations",component: ShowReservationsComponent , title:"Reservatios", canActivate:[noNavigateWithoutLoginGuard]},
+    ], canActivate:[noNavigateWithoutLoginGuard]},
 
-    {path: "UpdateUser", component:UpdateUserDetailsComponent, title:"Edit"},
+    {path: "UpdateUser", component:UpdateUserDetailsComponent, title:"Edit", canActivate:[noNavigateWithoutLoginGuard]},
     {path: "BreedSearch", component:BreedSearchComponent, title:"Search Breed"},
-    {path: 'Pet/details/:id', component: PetDetailsComponent,title:"Pet Details" },
-    { path: 'Pet/details/:id/:DoNotShowButton', component: PetDetailsComponent,title:"Pet Details"  },
+    {path: 'Pet/details/:id', component: PetDetailsComponent,title:"Pet Details", canActivate:[noNavigateWithoutLoginGuard] },
+    { path: 'Pet/details/:id/:DoNotShowButton', component: PetDetailsComponent,title:"Pet Details", canActivate:[noNavigateWithoutLoginGuard]},
     {path:"Vaccine",component:SearchVaccineComponent,title:"Vaccine"},
     {path: 'search-vaccine-clinic/:VaccineId', component: SearchVaccineClicnicComponent },
-    {path:"userPetEdit/:id",component:UserPetInfoEditComponent, title: "Edit Pet Information"},
-    {path: 'Clinic/:clinicId', component: ClinicComponent,title:"Clinic Details" },
-    {path:"ReservationVaccine/:clinicId/:VaccineId",component:ReservationForVaccineComponent,title:"Reservation Vaccine"},
+    {path:"userPetEdit/:id",component:UserPetInfoEditComponent, title: "Edit Pet Information", canActivate:[noNavigateWithoutLoginGuard]},
+    {path: 'Clinic/:clinicId', component: ClinicComponent,title:"Clinic Details", canActivate:[noNavigateWithoutLoginGuard] },
+    {path:"ReservationVaccine/:clinicId/:VaccineId",component:ReservationForVaccineComponent,title:"Reservation Vaccine", canActivate:[noNavigateWithoutLoginGuard]},
     {path:"clinics",component:ClinicSearchComponent,title:"Clinics"},
     {path:"vaccines",component:SearchVaccineClicnicComponent,title:"Vaccines"},
-    {path: 'secretary-vaccine/:ClinicId', component: SecretaryVaccineComponent ,title:"Secretary-vaccine"},
-    {path:"Secretary",component:SecretaryComponent,title:"Secretary"},
-    {path:"SecretaryClinic/:id",component:SecrteryclinicComponent,title:"Secretary"},
+    {path: 'secretary-vaccine/:ClinicId', component: SecretaryVaccineComponent ,title:"Secretary-vaccine", canActivate:[noNavigateWithoutLoginGuard]},
+    {path:"Secretary",component:SecretaryComponent,title:"Secretary", canActivate:[noNavigateWithoutLoginGuard]},
+    {path:"SecretaryClinic/:id",component:SecrteryclinicComponent,title:"Secretary", canActivate:[noNavigateWithoutLoginGuard]},
     {path:"",component:LandingPageComponent,title:"PetBook"},
-    {path: '**', redirectTo: '/Account'}
+    {path: '**', redirectTo: '/'}
 ];
 
